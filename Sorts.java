a6b8701a102bda8ed25aa721cc41ca28c99913ae
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-12-08 14:58:26 -0500

    Did bubble but have to fix other

--- Sorts.java
+++ Sorts.java
@@ -11,6 +11,7 @@ public class Sorts{
   for (;i <= end; i++){
 // for each place check if its smallest
     for (int x = 0; x + 1 <=end; x++){
+      
       // if the specified place is bigger than something switch the two.
       if (ary [x] <= ary [x +1]){
         small = ary [x];
@@ -23,7 +24,7 @@ public class Sorts{
 }
 public static String toString (int [] ary) {
   String yes = "";
-  for (int x = 0; x < ary.length - 1; x--){
+  for (int x = 0; x < ary.length - 1; x++){
     yes = yes + ary[x] + ", ";
   }
   return "[" + yes + ary [ary.length - 1] +"]";
@@ -31,13 +32,33 @@ public static String toString (int [] ary) {
 
 public static void main(String[] args) {
   Random randgen = new Random();
-  int[] test = new int[10];
-  for (int x = 0; x < 10; x++){
-    test[x] = randgen.nextInt() % 10;
+  int[] test = new int[20];
+  for (int x = 0; x < 20; x++){
+    test[x] = randgen.nextInt() % 20;
+}
+Random randge = new Random();
+int[] lol = new int[20];
+for (int x = 0; x < 20; x++){
+  lol[x] = randge.nextInt() % 20;
 }
 System.out.println(toString (test));
 selectionSort (test);
 System.out.println(toString (test));
+System.out.println("\n---------------------------------------------\n");
+System.out.println(toString (lol));
+selectionSort (lol);
+System.out.println(toString (lol));
 }
-public String Bubble (){;}
+public static void bubbleSort(int[] data){
+  int smallest = 0;
+for (int i = 0; i + 1 < data.length; i ++){
+    if (data[i] > data[i+1]){
+      smallest = data [i+1];
+      data [i+1] = data[i];
+      data[i] = smallest;
+    }
+
+} }
+
+
 }

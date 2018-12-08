e3db3aed38424a2a7f4ed6bdf6ac2629e5c0ed57
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-12-08 16:11:18 -0500

    Fixed first one, need to fix bubble now

--- Sorts.java
+++ Sorts.java
@@ -2,26 +2,25 @@ import java.util.Random;
 public class Sorts{
 
   public static void selectionSort(int [] ary) {
-  int end = ary.length -1;
-  int i = 0;
   int small = 0;
-  int l = 0;
-  int place;
-  String help = "";
-  for (;i <= end; i++){
+  int place = 0;
+  for (int i = 0; i < ary.length ; i++){
 // for each place check if its smallest
-    for (int x = 0; x + 1 <=end; x++){
-      
-      // if the specified place is bigger than something switch the two.
-      if (ary [x] <= ary [x +1]){
-        small = ary [x];
+        int smaller = ary [i];
+    for (int x = i  ; x < ary.length; x ++){
+      // if the specified place is smaller than the minimum.
+      if (smaller > ary [x]){
+        smaller = ary [x];
         place = x;
       }
-      // once the smallest one is found enter it into the String
-        help = help + ary [i];
+}
+      //Swapping
+System.out.println("the min = " + smaller + " the place is = " + place );
+      ary [place] = ary [i];
+      ary [i] = smaller;
     }
   }
-}
+
 public static String toString (int [] ary) {
   String yes = "";
   for (int x = 0; x < ary.length - 1; x++){
@@ -32,33 +31,44 @@ public static String toString (int [] ary) {
 
 public static void main(String[] args) {
   Random randgen = new Random();
-  int[] test = new int[20];
-  for (int x = 0; x < 20; x++){
-    test[x] = randgen.nextInt() % 20;
+  int[] test = new int[10];
+  for (int x = 0; x < 10; x++){
+    test[x] = randgen.nextInt() % 10;
 }
 Random randge = new Random();
-int[] lol = new int[20];
-for (int x = 0; x < 20; x++){
-  lol[x] = randge.nextInt() % 20;
+int[] lol = new int[1];
+for (int x = 0; x < 1; x++){
+  lol[x] = randge.nextInt() % 4;
 }
 System.out.println(toString (test));
 selectionSort (test);
 System.out.println(toString (test));
 System.out.println("\n---------------------------------------------\n");
 System.out.println(toString (lol));
-selectionSort (lol);
+bubbleSort (lol);
 System.out.println(toString (lol));
 }
 public static void bubbleSort(int[] data){
   int smallest = 0;
+  boolean perfect = true;
+  boolean allgood = false;
+  //checks to see if in right order
+  while(allgood == false){
+  for (int i = 0; i + 1< data.length; i ++){
+    if (data [i] > data [i+1]){
+      perfect = false;
+    }}
+    if (perfect){allgood = true;}
+    //Swapping
 for (int i = 0; i + 1 < data.length; i ++){
     if (data[i] > data[i+1]){
       smallest = data [i+1];
       data [i+1] = data[i];
       data[i] = smallest;
-    }
+    }}}}
 
-} }
+
+
 
 
 }

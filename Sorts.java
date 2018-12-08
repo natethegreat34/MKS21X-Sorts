ca3e0277e083a0997d99578f5861af1e241ac28a
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-12-08 17:53:08 -0500

    Figured out the problem; both are fixed now

--- Sorts.java
+++ Sorts.java
@@ -36,9 +36,9 @@ public static void main(String[] args) {
     test[x] = randgen.nextInt() % 10;
 }
 Random randge = new Random();
-int[] lol = new int[1];
-for (int x = 0; x < 1; x++){
-  lol[x] = randge.nextInt() % 4;
+int[] lol = new int[7];
+for (int x = 0; x < 7; x++){
+  lol[x] = randge.nextInt() % 10;
 }
 System.out.println(toString (test));
 selectionSort (test);
@@ -50,22 +50,30 @@ System.out.println(toString (lol));
 }
 public static void bubbleSort(int[] data){
   int smallest = 0;
-  boolean perfect = true;
+  boolean no = false;
   boolean allgood = false;
   //checks to see if in right order
-  while(allgood == false){
-  for (int i = 0; i + 1< data.length; i ++){
-    if (data [i] > data [i+1]){
-      perfect = false;
+for (int turns = 0; allgood == false; turns ++){
+    System.out.println("number of times " + turns);
+  for (int x = 0; x + 1< data.length; x ++){
+    if (data [x] > data [x+1]){
+      no = true;
     }}
-    if (perfect){allgood = true;}
+    System.out.println("no is " + no);
+    if (no == false){allgood = true;}
+
     //Swapping
+else{
 for (int i = 0; i + 1 < data.length; i ++){
     if (data[i] > data[i+1]){
       smallest = data [i+1];
       data [i+1] = data[i];
       data[i] = smallest;
-    }}}}
+        System.out.println( data [0] + " " + data [1] + " " + data [2] + " " + data [3] + " " + data [4] + " " + data [5] + " " + data [6] );
+  }}
+no = false;}
+}
+}
 
 
 
